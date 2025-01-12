12f8a91d4ab371322519cd3570aad7915ff06987
Pelayo <22166209@live.uem.es>
2025-01-12 22:26:40 +0100

    inicio función forecast
    procesamiento del dataset y creación del diccionario para el json

--- AsistenciaClase.py
+++ AsistenciaClase.py
@@ -153,3 +153,15 @@ def calcular_metricas_y_forecast_json(df, json_output):
     with open(json_output, "w", encoding="utf-8") as f:
         json.dump(data_json, f, ensure_ascii=False, indent=4)
     print(f"\nArchivo JSON generado en: {json_output}")
+
+def generar_forecast_por_asignatura(data):
+    data = data.withColumn("fecha", date_format(col("timestamp"), "yyyy-MM-dd"))
+
+    # Calcular totales diarios por estado
+    totales_diarios = data.groupBy("estado_asistencia", "fecha").agg(count("alumno").alias("total_diario"))
+    totales_pandas = totales_diarios.toPandas()
+    totales_pandas["fecha"] = pd.to_datetime(totales_pandas["fecha"])
+    totales_pandas = totales_pandas.sort_values(by=["estado_asistencia", "fecha"])
+
+    # Diccionario inicializado para almacenar resultados del forecast
+    forecast_resultados = {"presentes": 0, "tarde": 0, "ausentes": 0}
